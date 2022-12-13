3b11d522e7cb1c2559814d57a27200ddca563d04
Aviad Hadad <aviad@sthuck.info>
2022-12-13 02:38:09 +0200

    day11 finally

--- scala/src/main/scala/day11/day11.scala
+++ scala/src/main/scala/day11/day11.scala
@@ -5,14 +5,64 @@ import utils.Utils
 
 import scala.collection.mutable
 
+class Item(base: Int) {
+  val map: mutable.Map[Int, Int] = mutable.Map[Int, Int]()
+  for (n <- Item.divisibleByChecks) {
+    map(n) = base % n
+  }
+  def this(map: mutable.Map[Int, Int]) = {
+    this(0)
+    this.map ++= map
+  }
+
+  def isDivisibleBy(n: Int): Boolean = {
+    if (!map.contains(n)) {
+      throw new Exception(s"key ${n} not found")
+    }
+    map(n) == 0
+  }
+
+  def +(i: Int) = {
+    val newMap = mutable.Map[Int, Int]()
+    for (n <- Item.divisibleByChecks) {
+      newMap(n) = (this.map(n) + i) % n
+    }
+    Item(newMap)
+  }
+
+  def square() = {
+    val newMap = mutable.Map[Int, Int]()
+    for (n <- Item.divisibleByChecks) {
+      newMap(n) = (this.map(n) * this.map(n)) % n
+    }
+    Item(newMap)
+  }
+
+  def *(i: Int) = {
+    val newMap = mutable.Map[Int, Int]()
+    for (n <- Item.divisibleByChecks) {
+      newMap(n) = (this.map(n) * i) % n
+    }
+    Item(newMap)
+  }
+}
+
+object Item {
+  private var divisibleByChecks: mutable.Seq[Int] = mutable.ArraySeq[Int]()
+  def registerDivisibleBy(n: Int): Unit = {
+    divisibleByChecks = divisibleByChecks.appended(n)
+  }
+}
+
+
 case class Move(
-  item: BigInt,
+  item: Item,
   moveTo: Int
                )
 case class Monkey(val id: Int,
-                  var items: mutable.Stack[BigInt],
-                  val operation: (BigInt) => BigInt,
-                  val testDivisibleBy: BigInt,
+                  var items: mutable.Stack[Item],
+                  val operation: (Item) => Item,
+                  val testDivisibleBy: Int,
                   val targetTrue: Int,
                   val targetFalse: Int) {
 private var inspectedItems = 0
@@ -29,7 +79,7 @@ private var inspectedItems = 0
 //      val newWorryBoredom = Math.round(newWorry/3)
       val newWorryBoredom = newWorry
 //      println(s"\t\tMonkey gets bored with item. Worry level is divided by 3 to ${newWorryBoredom}")
-      val isDivisble = newWorryBoredom % this.testDivisibleBy == 0
+      val isDivisble = newWorryBoredom.isDivisibleBy(this.testDivisibleBy)
 //      println(s"\t\tCurrent worry level is ${if isDivisble then "" else "not"} divisible by ${this.testDivisibleBy}")
       val target = if isDivisble then targetTrue else targetFalse
 //      println(s"\t\tItem with worry level ${item} is thrown to monkey ${target}.")
@@ -37,43 +87,40 @@ private var inspectedItems = 0
     }
     moves
   }
-  def removeItem(item: BigInt) = this.items = this.items.filter(_ == item)
-  def addItem(item: BigInt) = this.items = this.items :+ item
+  def removeItem(item: Item): Unit = this.items = this.items.filter(_ == item)
+  def addItem(item: Item): Unit = this.items = this.items :+ item
 
-  def getTotalInspected = inspectedItems
+  def getTotalInspected: Int = inspectedItems
 }
 
 object Monkey {
   def parse(input: List[String]): Monkey = {
-    /*
-    Monkey 0:
-      Starting items: 56, 56, 92, 65, 71, 61, 79
-      Operation: new = old * 7
-      Test: divisible by 3
-        If true: throw to monkey 3
-        If false: throw to monkey 7
-    */
+
     val id = "Monkey (\\d):".r.findFirstMatchIn(input.head).get.group(1).toInt
     val items = input(1).split(": ")(1).split(", ").map(_.toInt).toSeq
-    val itemsStack = mutable.Stack[BigInt]()
-    itemsStack.pushAll(items.map(BigInt(_)))
+
     val operationMatches = "Operation: new = old ([*+]) (\\d+|old)".r.findFirstMatchIn(input(2)).get
     val operand2 = operationMatches.group(2)
     val operation = operationMatches.group(1) match {
-      case "*" => (x: BigInt) => x * (if (operand2 == "old") then x else operand2.toInt)
-      case "+" => (x: BigInt) => x + (if (operand2 == "old") then x else operand2.toInt)
+      case "*" => (x: Item) =>  if (operand2 == "old") then {
+        x.square()
+      } else x * operand2.toInt
+      case "+" => (x: Item) => if (operand2 == "old") then {
+        throw new Exception("not implemented")
+      } else x + operand2.toInt
     }
     val testDivisibleBy = "Test: divisible by (\\d+)".r.findFirstMatchIn(input(3)).get.group(1).toInt
     val targetTrue = "If true: throw to monkey (\\d+)".r.findFirstMatchIn(input(4)).get.group(1).toInt
     val targetFalse = "If false: throw to monkey (\\d+)".r.findFirstMatchIn(input(5)).get.group(1).toInt
-
+    val itemsStack = mutable.Stack[Item]()
+    itemsStack.pushAll(items.map(Item(_)))
     Monkey(id, itemsStack, operation, testDivisibleBy, targetTrue, targetFalse)
   }
 }
 
 class Manager(private val monkeys: List[Monkey]) {
   private def applyMove(originalMonkey: Monkey, move: Move): Unit = {
-    originalMonkey.removeItem(move.item)
+//    originalMonkey.removeItem(move.item)
     this.monkeys.find(_.id == move.moveTo).get.addItem(move.item)
   }
   def doRound(): Unit = {
@@ -84,14 +131,19 @@ class Manager(private val monkeys: List[Monkey]) {
   }
 
   def monkeyBuisness() = {
-    monkeys.map(_.getTotalInspected).sorted.takeRight(2)
+    monkeys.map(_.getTotalInspected).sorted.takeRight(2).map(_.toLong).product
   }
 }
 
-object day10 {
-  @main def part1(): Unit = {
+object day11 {
+  //part 1 got lost in the refactoring for part2, because "Item" class is one giant hack
+  @main def part2(): Unit = {
     val input = Utils.readFromFile("src/main/resources/day11-input")
     val chunks = Utils.sliceLinesToChunks(input, _ == "")
+    chunks.foreach(chunk => {
+      val testDivisibleBy = "Test: divisible by (\\d+)".r.findFirstMatchIn(chunk(3)).get.group(1).toInt
+      Item.registerDivisibleBy(testDivisibleBy)
+    })
     val monkeys = chunks.map(Monkey.parse)
     val manager = Manager(monkeys)
 
